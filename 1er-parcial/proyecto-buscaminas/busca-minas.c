30a72385bb2d6de250bd9d2e31fc16ba1066eb1d
mabby <rznns.mabby@gmail.com>
2023-06-06 10:59:28 -0400

    buscaminas

---
+++ 1er-parcial/proyecto-buscaminas/busca-minas.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 10
+#define MINAS 5
+
+void ver(char grid[SIZE+2][SIZE+2]){
+    for(int j = 0; j<SIZE+2; j++){
+        for(int i = 0; i<SIZE+2; i++){
+            if(grid[j][i] == '\0'){
+                printf(".");
+            } else{
+                printf("%c  ", grid[j][i]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+void seleccionar(int *x, int *y){
+    printf("x: ");
+    scanf("%d", x);
+
+    printf("y: ");
+    scanf("%d", y);
+}
+
+int checar(char grid[SIZE+2][SIZE+2],int x,int y){
+
+
+
+    return 0;
+}
+
+int main(){
+    int coor_x, coor_y;
+
+    // inicializar arreglo vacio
+    char grid[SIZE+2][SIZE+2] = {
+        {""}
+    };
+
+    for(int i = 0; i<MINAS; i++){
+        int x = rand() % SIZE+1;
+        int y = rand() % SIZE+1;
+
+        grid[y][x] = 'x';
+    }
+    
+    // printf mapa
+    ver(grid);
+
+    // seleccionar casilla
+    seleccionar(&coor_x, &coor_y);
+
+    // checar al rededor
+    checar(grid, coor_x, coor_y);
+
+    return 0;
+}
